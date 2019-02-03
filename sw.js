b47b19ad6c8ce639f1d2ae04f30e00a043f331dd
Mykal Morton <mydates@gmail.com>
2019-02-03 15:34:17 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,84 +1,58 @@
-/* Stores the Service Worker cache and
+* Stores the Service Worker cache and
 offline capabilities
 This follows the MDN tutorial @:
 https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 */
-let staticCacheName = 'fend-rrapp-v01';
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
 
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(staticCacheName).then(cache => {
-      return cache.addAll([
-        '/',
-        '/serviceworker.js',
-        '/index.html',
-        '/restaurant.html',
-        '/css/styles.css',
-        '/js/',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/data/restaurants.json',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
-        '/img/10.jpg',
-        '/img/fallback.jpg',
-        '/restaurant.html?id=1',
-        '/restaurant.html?id=2',
-        '/restaurant.html?id=3',
-        '/restaurant.html?id=4',
-        '/restaurant.html?id=5',
-        '/restaurant.html?id=6',
-        '/restaurant.html?id=7',
-        '/restaurant.html?id=8',
-        '/restaurant.html?id=9',
-        '/restaurant.html?id=10'
-      ]);
-    })
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+      caches.open('v1').then(function(cache) {
+          return cache.addAll(cacheFiles);
+      })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName.startsWith('fend-') &&
-                 cacheName != staticCacheName;
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+      caches.match(e.request).then(function(response) {
+          if (response) {
+              console.log('Found ', e.request, ' in cache');
+              return response;
+          }
+          else {
+              console.log('Could not find ', e.request, ' in cache, FETCHING!');
+              return fetch(e.request)
+              .then(function(response) {
+                  const clonedResponse = response.clone(); 
+                  caches.open('v1').then(function(cache) {
+                      cache.put(e.request, clonedResponse);
+                  })
+                  return response;
+              })
+              .catch(function(err) {
+                  console.error(err);
+              });
+          }
+      })
   );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request).then(response => {
-        let responseClone = response.clone();
-        caches.open(staticCacheName).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      });
-    }).catch(function() {
-      return caches.match('/img/fallback.jpg');
-    })
-  );
-});
-
-self.addEventListener('message', event => {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
